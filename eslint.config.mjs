b7db8d9e2660a01c4ad9cd5f1c7fa0d0d3c8833e
SeaotterMS <peter910820@gmail.com>
2025-11-15 12:29:45 +0800

    chore: code consistency settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  // Prettier 支援
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      "prettier/prettier": ["error"], // 將 Prettier 規則當成 ESLint 錯誤
+    },
+    extends: [prettier], // 關閉與 Prettier 衝突的 ESLint 規則
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
